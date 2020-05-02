8a1d49dde36860e1ef31f063bacee2ba3c87dadd
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-05-02 23:44:11 +0200

    Fix git issue

---
+++ client/src/components/chat/Chat.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import "./Chat.css";
+import InfoBar from "../InfoBar/InfoBar";
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name, room }, (error) => {
+      alert(error);
+    });
+    return () => {
+      socket.emit("disconnent");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  //function for sending messages
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        {/* <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
